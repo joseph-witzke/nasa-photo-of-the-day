fe8784768fa8e128819524c97f74dea5eb5451e7
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-14 15:40:20 -0600

    Initial commit with axios imported and some begining elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
+
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        console.log(res)
+        setNasaData(res.data)
+      })
+      .catch( err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
+    <>
+    <header>
+      <nav></nav>
+      <nav></nav>
+      <nav></nav>
+    </header>
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>{nasaData.title}</h1>
+        <img src={nasaData.hdurl}/>
     </div>
+    <div>
+
+    </div>
+    </>
   );
 }
 
